04e334aeaefa422b107f599e811542a8214e1cb3
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-03-23 23:58:10 +0300

    refactor: adjust HTML-formatting according to rule 3.4 in CSS

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit";
 
     deleteButton.innerText="Delete";
-    deleteButton.className="delete";
+    deleteButton.className="del";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
